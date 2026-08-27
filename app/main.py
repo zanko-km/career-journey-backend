c21b5e8a0b127ddbcaea728eb375870a5a19d873
zanko-km <zankokarimy@gmail.com>
2026-08-27 11:50:03 +0330

    Feat: add me/profile endpoint

--- app/main.py
+++ app/main.py
@@ -1,14 +1,15 @@
 from fastapi import FastAPI
+
+from app.api.routes import competencies
 from app.api.routes.auth import router as auth_router
-from app.api.routes.teams import router as teams_router
-from app.core.exceptions import APIException
-from app.core.exception_handlers import api_exception_handler
+from app.api.routes.competency_cycles import router as competency_cycle_router
 from app.api.routes.employees import router as employee_router
 from app.api.routes.me import router as me_router
 from app.api.routes.meetings import router as meeting_router
-from app.api.routes import competencies
-from app.api.routes.competency_cycles import router as competency_cycle_router
 from app.api.routes.notification import router as notif_router
+from app.api.routes.teams import router as teams_router
+from app.core.exception_handlers import api_exception_handler
+from app.core.exceptions import APIException
 
 tags_metadata = [
     {
--- app/models/position.py
+++ app/models/position.py
@@ -1,5 +1,7 @@
 from sqlalchemy.orm import Mapped, mapped_column, relationship
+
 from app.core.database import Base
+
 
 class Position(Base):
     __tablename__ = "positions"
